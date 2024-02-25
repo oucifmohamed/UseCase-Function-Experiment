fa10d2e4c2d202c851b3727f5b614051ace29fe0
mohammedoucifyassir <105277297+mohammedoucifyassir@users.noreply.github.com>
2024-02-25 16:06:00 +0100

    Use typealias for the use-case function in the DI file

--- src/main/kotlin/di/DIModule.kt
+++ src/main/kotlin/di/DIModule.kt
@@ -2,18 +2,19 @@ package di
 
 import presenter.Presenter
 import repository.Repository
+import usecase.FetchClientsUseCase
 import usecase.fetchClientsUseCase
 
 fun getPresenter(): Presenter {
     return Presenter(getUseCase())
 }
 
-fun getUseCase(): () -> List<String> {
+fun getUseCase(): FetchClientsUseCase {
     return {
         fetchClientsUseCase(getRepository())
     }
 }
 
 fun getRepository(): Repository {
-    return Repository();
+    return Repository()
 }
